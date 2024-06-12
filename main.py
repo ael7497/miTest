c610d7bb90cf65450d2eb76d93bf0b4ea0542271
Some-O-ne <sasha.go.go.go.go@gmail.com>
2024-06-12 18:58:57 +0300

    added favicons and minorly improved test page

--- main.py
+++ main.py
@@ -32,27 +32,38 @@ def test():
     question = data[0][1]
 
 
-    return render_template("test.html",question=question)
+    return render_template("test.html",question=question,statementID=session["statementID"])
 
 
 
 @app.route("/back")
 def back():
     if session["statementID"] < 1: return
+    intellect = session["last"]["intellect"]
+
+    session["intellectScores"][intellect] -= session["last"]["answer"]
+
+    if session["intellectScores"][intellect] < 0: session["intellectScores"][intellect] = 0
     session["statementID"] -= 1
     return redirect("/test")
 
 @app.route("/answer")
 def next():
     answer = int(request.args.get('value'))
+    
     db.connect()
     data = db.get(get_by_id, [session["statementID"]])
     db.disconnect()
+    
     if not data:
         return redirect("/result")
     intellect = data[0][2]
     session["intellectScores"][intellect] += answer
+
+    session["last"] = {"intellect":intellect,"answer":answer}
+
     if session["intellectScores"][intellect] < 0: session["intellectScores"][intellect] = 0
+
     session["statementID"] += 1
     return redirect("/test")
     
@@ -60,6 +71,7 @@ def next():
 @app.route('/result')
 def result():
     session['statementID'] = 1
+    
     return '<h2>result</h2>'+str(session["intellectScores"])
 
 if __name__ == '__main__':
--- static/js/test.js
+++ static/js/test.js
@@ -1,7 +1,13 @@
-buttons = [...document.getElementsByClassName("answer")]
-buttons.forEach(button => {
+answerbButtons = [...document.getElementsByClassName("answer")]
+answerbButtons.forEach(button => {
     button.addEventListener("click",(event)=>{
         window.location.href= `/answer?value=${event.target.value}`
     })
     
 });
+backButton = document.getElementsByClassName("back")
+if (backButton.length) {
+    backButton[0].addEventListener("click",()=>{
+        window.location.href = "/back"
+    })
+}
