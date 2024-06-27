1a50eb7125e36bb1d5a2893432817ffcdacd086c
Some-O-ne <sasha.go.go.go.go@gmail.com>
2024-06-27 23:54:41 +0300

    remade summaries

--- static/js/result.js
+++ static/js/result.js
@@ -1,22 +1,22 @@
 const intellectCard = document.getElementsByClassName("card intellect")[0]
 const personalityCard = document.getElementsByClassName("card personality")[0]
 
-console.log(intellectCard,personalityCard)
+function setCard(card,element) {
+    const map = {"personality":"личности","intellect":"интеллекта"}
+    console.log(card,element)
+    card.innerHTML=`<h1>Тип ${map[[...element.classList].find((el)=>{return el != "hoverable"})]}: <span>${element.innerText}</span></h1>
+        <p>${summaries[element.innerText]}</p>
+    `;
+}
 
 const summarizable = [...document.querySelectorAll(".hoverable")];
 summarizable.forEach((element)=>{
-    if (element.classList.contains("intellect")) {
-        element.addEventListener("click",(target)=>{
-            intellectCard.innerText = summaries[target.target.innerText];
+    card = element.classList.contains("intellect") ? intellectCard : personalityCard;
+    element.addEventListener("click",()=>{
+        setCard(card,element);
 
-        });}
-    else{
-        element.addEventListener("click",(target)=>{
-                personalityCard.innerText = summaries[target.target.innerText];
-    
-            });
+        });
     }
-}
 )
-intellectCard.innerText = summaries[summarizable.find((el)=>{return el.classList.contains("intellect")}).innerText];
-personalityCard.innerText = summaries[summarizable.find((el)=>{return el.classList.contains("personality")}).innerText];
+setCard(intellectCard,summarizable.find((el)=>{return el.classList.contains("intellect")}));
+setCard(personalityCard,summarizable.find((el)=>{return el.classList.contains("personality")}));
