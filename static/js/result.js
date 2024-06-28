4be44f9bdf3e8ac83aca1425a84984fc54122c3d
Some-O-ne <sasha.go.go.go.go@gmail.com>
2024-06-28 21:08:03 +0300

    deleted debug prints

--- static/js/result.js
+++ static/js/result.js
@@ -3,7 +3,6 @@ const personalityCard = document.getElementsByClassName("card personality")[0]
 
 function setCard(card,element) {
     const map = {"personality":"личности","intellect":"интеллекта"}
-    console.log(card,element)
     card.innerHTML=`<h1>Тип ${map[[...element.classList].find((el)=>{return el != "hoverable"})]}: <span>${element.innerText}</span></h1>
         <p>${summaries[element.innerText]}</p>
     `;
@@ -11,10 +10,8 @@ function setCard(card,element) {
 
 const summarizable = [...document.querySelectorAll(".hoverable")];
 summarizable.forEach((element)=>{
-    console.log(element)
     let card = element.classList.contains("intellect") ? intellectCard : personalityCard;
-    console.log(element.classList.contains("intellect") ? intellectCard : personalityCard)
-    console.log(card)
+
     element.addEventListener("click",()=>{
         setCard(card,element);
 
